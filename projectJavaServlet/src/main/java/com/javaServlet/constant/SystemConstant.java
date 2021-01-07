7845d399f66bd2703dd61c226a4859a28b08a3eb
Long Trinh <longtrinhvan97@gmail.com>
2021-01-07 09:46:06 +0700

    test

--- projectJavaServlet/src/main/java/com/javaServlet/constant/SystemConstant.java
+++ projectJavaServlet/src/main/java/com/javaServlet/constant/SystemConstant.java
@@ -2,10 +2,11 @@ package com.javaServlet.constant;
 
 public class SystemConstant {
 
-//	public static final String MODEL = "model";
-//	public static final String ADMIN_ = "ADMIN";
-//	public static final String USER = "USER";
-//	public static final String LIST = "list";
-//	public static final String EDIT = "edit";
+	public static final String MODEL = "model";
+	public static final String ADMIN_ = "ADMIN";
+	public static final String USER = "USER";
+	public static final String LIST = "list";
+	public static final String EDIT = "edit";
 
+	
 }
