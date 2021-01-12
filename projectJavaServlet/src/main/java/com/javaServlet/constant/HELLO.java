211f6aa7cfb709bea272f9f1793b8f973e757c1d
Long <67250057+longtrinhvan@users.noreply.github.com>
2021-01-12 13:38:02 +0700

    Update HELLO.java
    ok

--- projectJavaServlet/src/main/java/com/javaServlet/constant/HELLO.java
+++ projectJavaServlet/src/main/java/com/javaServlet/constant/HELLO.java
@@ -3,6 +3,12 @@ package com.javaServlet.constant;
 public class HELLO {
 	public static void main(String[] args) {
 		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
 	}
 	
 }
