65a0d9578937e3fa5e94f7bda0ffeeac15279062
Long <67250057+longtrinhvan@users.noreply.github.com>
2021-01-12 13:27:31 +0700

    Update HELLO.java
    pro

--- projectJavaServlet/src/main/java/com/javaServlet/constant/HELLO.java
+++ projectJavaServlet/src/main/java/com/javaServlet/constant/HELLO.java
@@ -4,12 +4,6 @@ public class HELLO {
 	public static void main(String[] args) {
 		System.out.println();
 		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
 	}
 	
 }
